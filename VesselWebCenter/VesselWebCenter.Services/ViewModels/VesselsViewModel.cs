a98cb22ac91148ee2e5b28d6702d0de31ffcf0b6
Atanas Atanasov <djx700@abv.bg>
2022-12-01 18:45:37 +0200

    Vessel ViewModel - mododified

--- VesselWebCenter/VesselWebCenter.Services/ViewModels/VesselsViewModel.cs
+++ VesselWebCenter/VesselWebCenter.Services/ViewModels/VesselsViewModel.cs
@@ -7,14 +7,10 @@ namespace VesselWebCenter.Services.ViewModels
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-
         public string  CallSign { get; set; } = null!;
-
         public VesselType VesselType { get; set; }
-
         public int LOA { get; set; }
         public int Breadth { get; set; }
-
         public bool VesselAvailableForVoyage { get; set; } = false;
         public List<PortOfCall>? PortsOfCall { get; set; }
     }
